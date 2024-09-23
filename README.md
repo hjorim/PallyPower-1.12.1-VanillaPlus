d1fd8a63ef76ec3b9ff3af9f350e59dee79b812e
hjorim <137732862+hjorim@users.noreply.github.com>
2024-09-23 02:31:53 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# PallyPower VanillaPlus
+
+How to install: Download the zip file and rename to PallyPower
+
+Changes:
+- Included an option to change between Regular Blessings and Greater Blessings.
+- Shows the buff frame when solo
+- Included Pet in the buff table
+- Show the max rank of each blessing each paladin has available + if they have talents that buff the blessing (specific to v+)
+- Show the correct duration to each blessing based on v+ duration
---
+++ localization.lua
@@ -0,0 +1,77 @@
+PallyPower_Version = "1.7"
+SLASH_PALLYPOWER1 = "/pp"
+SLASH_PALLYPOWER2 = "/pallypower"
+
+PallyPower_BlessingID = { };
+PallyPower_BlessingID[0] = "Wisdom";
+PallyPower_BlessingID[1] = "Might";
+PallyPower_BlessingID[2] = "Salvation";
+PallyPower_BlessingID[3] = "Light";
+PallyPower_BlessingID[4] = "Kings";
+PallyPower_BlessingID[5] = "Sanctuary";
+
+PallyPower_DivineGraceTalentSearch = "Divine Grace";
+PallyPower_GuardianFavorTalentSearch = "Guardian's Favor";
+PallyPower_DivineMightTalentSearch = "Divine Might";
+PallyPower_SanctuaryTalentSearch = "Blessing of Sanctuary";
+
+if (RegularBlessings == false) 
+  then
+    PallyPower_BlessingSpellSearch = "Greater Blessing of (.*)";
+  else
+    PallyPower_BlessingSpellSearch = "Blessing of (.*)";
+end
+
+PallyPower_Rank1 = "Rank 1"
+PallyPower_RankSearch = "Rank (.*)"
+PallyPower_Symbol = "Symbol of Kings"
+
+-- _,class = UnitClass("player") returns....
+PallyPower_Paladin = "PALADIN"
+
+-- Used... ClassID .. ": Blessing of "..BlessingID
+PallyPower_BuffFrameText = ": Blessing of "
+PallyPower_Have = "Have: "
+PallyPower_Need = "Need: "
+PallyPower_NotHere = "Not Here: "
+PallyPower_Dead = "Dead: "
+
+PallyPower_BuffBarTitle = "Pally Buffs (%d)"
+
+--- By Lines... Keep People the same, feel free to add yourself in the _Credits3 line if your localizing
+--- And feel free to add a friend or two to special thanks
+PallyPower_Credits1 = "Pally Power - by Aznamir"
+PallyPower_Credits2 = "Version "..PallyPower_Version
+PallyPower_Credits3 = ""
+PallyPower_Credits4 = "Original update by Sneakyfoot / Rake / Xerron / Azgaardian"
+PallyPower_Credits5 = "Updated version for Vanilla+ including Pets and other improvements by Hjorim"
+
+-- Buff name, Class Name
+PallyPower_CouldntFind = "Couldn't find a target for %s on %s!"
+
+-- Buff name, Class name, Person Name
+PallyPower_Casting = "Casting %s on %s (%s)"
+
+ PallyPower_ClassID = { };
+PallyPower_ClassID[0] = "Warrior";
+PallyPower_ClassID[1] = "Rogue";
+PallyPower_ClassID[2] = "Priest";
+PallyPower_ClassID[3] = "Druid";
+PallyPower_ClassID[4] = "Paladin";
+PallyPower_ClassID[5] = "Hunter";
+PallyPower_ClassID[6] = "Mage";
+PallyPower_ClassID[7] = "Warlock";
+PallyPower_ClassID[8] = "Shaman";
+PallyPower_ClassID[9] = "Pet";
+
+--XML
+PALLYPOWER_CLEAR = "Clear";
+PALLYPOWER_REFRESH = "Refresh";
+PALLYPOWER_RESETPOSITION = "Reset Position";
+PALLYPOWER_OPTIONS = "Options";
+PALLYPOWER_OPTIONS_TITLE = "Pally Power Options";
+PALLYPOWER_OPTIONS_SCAN = "Scan Frequency (seconds):";
+PALLYPOWER_OPTIONS_SCAN2 = "Poll Per Frame: ";
+PALLYPOWER_OPTIONS_FEEDBACK_CHAT = "Show feedback in chat";
+PALLYPOWER_OPTIONS_SMARTBUFFS = "Smart Buffs";
+PALLYPOWER_OPTIONS_REGULAR = "Enable Regular Blessing Only - NO GREATER BLESSINGS";
